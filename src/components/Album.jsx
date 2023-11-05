d2e300793260860a27ed51b707fd6f9ce6b197c1
linaayson <andersson.lina89@gmail.com>
2023-11-05 20:31:48 +0100

    fontsize and color update

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -10,31 +10,31 @@ import Dots from "./DotIcon";
 export default function Album({ albumData }) {
   return (
     <div className="album">
-      <div className="cover-wrapper">
-        <CoverImage imageUrl={albumData.images[0].url} />
-        <span className="icons-wrapper">
-          <button className="icon">
-            <Heart />
-          </button>
-          <button className="icon-play">
-            <Play />
-          </button>
-          <button className="icon">
-            <Dots />
-          </button>
-        </span>
-      </div>
-
-      <div className="album-info">
-        <AlbumName
-          name={albumData.name}
-          externalUrl={albumData.external_urls.spotify}
-        />
-
-        <div className="artist-name">
-          <ArtistName artists={albumData.artists} />
-        </div>
-      </div>
+    <div className="cover-wrapper">
+      <CoverImage imageUrl={albumData.images[0].url} />
+      <span className="icons-wrapper">
+        <button className="icon">
+          <Heart />
+        </button>
+        <button className="icon-play">
+          <Play />
+        </button>
+        <button className="icon">
+          <Dots />
+        </button>
+      </span>
     </div>
+
+    <div className="album-info">
+      <AlbumName
+        name={albumData.name}
+        externalUrl={albumData.external_urls.spotify}
+      />
+
+<div className="artist-name">
+  <ArtistName artists={albumData.artists} />
+</div>
+    </div>
+  </div>
   );
 }
--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -2,18 +2,17 @@ import React from "react";
 
 export default function ArtistName({ artists }) {
   return (
-    <div className="artist-names">
-      {artists.map((artist, index) => (
-        <span key={index}>
-          <a
-            href={artist.external_urls.spotify}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h3>{artist.name}</h3>
-          </a>
-        </span>
-      ))}
-    </div>
+    <span className="artist-names">
+    {artists.map((artist, index) => (
+      <a
+        key={index}
+        href={artist.external_urls.spotify}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h3>{artist.name}</h3>
+      </a>
+    ))}
+  </span>
   );
 }
